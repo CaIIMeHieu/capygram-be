b52ba22eb0d82f82285b8a1d5eab05ce9710ef58
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2024-10-18 17:05:22 +0700

    test cicd

--- run_capygram_docker.sh
+++ run_capygram_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "-------------------------Remove container--------------------------------"
-sudo docker compose -f .\docker-compose.Dev.Infrastructure.yaml down
+echo $SUDO_PASSWORD | sudo -S docker compose -f ./docker-compose.Dev.Infrastructure.yaml down
 echo "-------------------------Rebuild image--------------------------------"
-sudo docker-compose -f .\docker-compose.Dev.Infrastructure.yaml build --no-cache
+echo $SUDO_PASSWORD | sudo -S docker-compose -f ./docker-compose.Dev.Infrastructure.yaml build --no-cache
 echo "-------------------------Run container--------------------------------"
-sudo docker compose -f .\docker-compose.Dev.Infrastructure.yaml up -d
+echo $SUDO_PASSWORD | sudo -S docker compose -f ./docker-compose.Dev.Infrastructure.yaml up -d

43d4e816eeb669d8a247a97ad6f50e9187c4d92f
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2024-10-21 18:35:43 +0700

    edit notification

--- src/Services/capygram.Notification/Program.cs
+++ src/Services/capygram.Notification/Program.cs
@@ -1,20 +1,34 @@
+using capygram.Common.DependencyInjection.Extensions;
+using capygram.Notification.DependencyInjection.Extensions;
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Notification.Development.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var config = builder.Configuration;
+builder.Services
+    .ConfigurationMediatR(typeof(Program).Assembly)
+    .AddServices()
+    .AddConfigurationOption(config)
+    .ConfigurationMasstransit(config);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
+//}
 
 app.UseHttpsRedirection();
 
